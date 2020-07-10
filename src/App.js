de2caf3344bacd84e634f27a65fe07e283f17d25
Fsskay <fsskay@126.com>
2020-07-10 11:20:18 +0800

    制作账本的priceList 使用ionicons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import PriceList from "./components/PriceList";
+const items = [
+    {
+        "id":1,
+        "title":"去云南旅游",
+        "price":200,
+        "category":{"id":1,"name":"旅行","type":"outcome","iconName":"ios-plane"}
+    },
+    {
+        "id":2,
+        "title":"去云南旅游",
+        "price":400,
+        "category":{"id":2,"name":"旅行","type":"outcome","iconName":"ios-plane"}
+    }
+];
 
 function App() {
   return (
@@ -19,6 +35,11 @@ function App() {
           Learn React
         </a>
       </header>
+        <PriceList
+            items={items}
+            onModifyItem={(item)=>{alert(item.id)}}
+            onDeleteItem={(item)=>{alert(item.id)}}
+        />
     </div>
   );
 }
