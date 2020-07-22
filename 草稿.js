dff5050e3eaf4b5f0ddd19ef69b4a727b19618df
Fsskay <fsskay@126.com>
2020-07-23 00:15:50 +0800

    React16组件化+测试+全流程            实战“在线账本”项目            第四章 第一节 使用容器型组件Home.js将先前的组件整合

---
+++ 草稿.js
@@ -0,0 +1,11 @@
+class MonthPicker extends React.component{
+    constructor(props) {
+        super(props);
+        this.state={isOpen:false,selectedyear,this.prosp.year}
+    }
+
+    hanshuA=(event)=>{
+        e.preventDefault();
+        this.setState({isOpen})
+    }
+}
