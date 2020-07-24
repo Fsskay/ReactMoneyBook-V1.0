ae8575d7b3824927acb350cf7836536f2423fd5d
Fsskay <fsskay@126.com>
2020-07-24 18:12:10 +0800

    React16组件化+测试+全流程            实战“在线账本”项目            第四章 第二节 分析设计State结构            初步设计home.js的state,对items数据进行整理,分离出cid数据,使用itemsWithCategory代替item.category.新建parseToYearAndMonth,

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,43 +1,72 @@
 import React, {Component} from 'react';
 import logo from '../logo.svg';
 
-import {LIST_VIEW, CHART_VIEW, TYPE_OUTCOME} from '../utility'
+import {LIST_VIEW, CHART_VIEW, TYPE_OUTCOME, parseToYearAndMonth} from '../utility'
 import PriceList from "../components/PriceList";
 import ViewTab from "../components/ViewTab";
 import TotalPrice from "../components/TotalPrice";
 import MonthPicker from "../components/MonthPicker"
 
+
+const categoies = {
+    "1": {
+        "id": 1, "name": "旅行", "type": "outcome", "iconName": "ios-plane"
+    },
+    "2": {
+        "id": 2, "name": "理财", "type": "income", "iconName": "ios-plane"
+    }
+};
 const items = [
     {
         "id": 1,
         "title": "去云南旅游",
         "price": 200,
-        "category": {"id": 1, "name": "旅行", "type": "outcome", "iconName": "ios-plane"}
+        "date": "2020年01月01日",
+        "cid": 1
     },
     {
         "id": 2,
         "title": "去云南旅游",
         "price": 400,
-        "category": {"id": 2, "name": "旅行", "type": "outcome", "iconName": "ios-plane"}
+        "date": "2020年01月02日",
+        "cid": 1
+
     },
     {
         "id": 3,
         "title": "理财",
         "price": 1000,
-        "category": {"id": 3, "name": "理财", "type": "income", "iconName": "ios-plane"}
+        "date": "2020年01月03日",
+        "cid": 2
     }
 ];
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items,
+            currentDate: parseToYearAndMonth(),
+            tabView: LIST_VIEW,
+        }
+    }
+
     render() {
+        const {items, currentDate, tabView} = this.state;
+        const itemsWithCategory = items.map(item => {
+            item.category = categoies[item.cid];
+            return item
+        });
+
         let totalIncome = 0, totalOutcome = 0;
-        items.forEach(item=>{
+        itemsWithCategory.forEach(item => {
             if (item.category.type === TYPE_OUTCOME) {
                 totalOutcome += item.price
             } else {
                 totalIncome += item.price
             }
         });
+
         return (
             <React.Fragment>
                 <header className="App-header">
@@ -47,8 +76,8 @@ class Home extends Component {
                     <div className="row">
                         <div className="col">
                             <MonthPicker
-                                year={2018}
-                                month={8}
+                                year={currentDate.year}
+                                month={currentDate.month}
                                 onChange={() => {
                                 }}
                             />
@@ -60,16 +89,17 @@ class Home extends Component {
                             />
                         </div>
                     </div>
-
                 </header>
                 <div className="content-area py-3 px-3">
-                        <ViewTab activeTab={LIST_VIEW} onTabChange={()=>{}}/>
-                        <creatBtn onClick={()=>{}}/>
-                        <PriceList onModifyItem={()=>{}} items={items} onDeleteItem={()=>{}}/>
+                    <ViewTab activeTab={tabView} onTabChange={() => {
+                    }}/>
+                    <creatBtn onClick={() => {
+                    }}/>
+                    <PriceList onModifyItem={() => {}} items={itemsWithCategory} onDeleteItem={() => {
+                    }}/>
                 </div>
             </React.Fragment>
         )
-
     }
 }
 
--- src/utility.js
+++ src/utility.js
@@ -4,15 +4,22 @@ export const TYPE_INCOME = 'income';
 export const TYPE_OUTCOME = 'outcome';
 
 
-
 export const padLeft = (n) => {
     return n < 10 ? '0' + n : n
 };
 
-export const range = (size,startAt = 0)=> {
-    const arr = []
-    for(let i=0;i<size;i++){
+export const range = (size, startAt = 0) => {
+    const arr = [];
+    for (let i = 0; i < size; i++) {
         arr[i] = startAt + i
     }
     return arr
-}
+};
+
+export const parseToYearAndMonth = (str) => {
+    const date = str ? new Date(str) : new Date();
+    return {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+    }
+};
